1ed47a33381a19dbdb38a7362bc9c371c0b3d526
priyaank <priyaankk@gmail.com>
2023-05-03 18:03:26 +0530

    Ui changes

--- src/components/LoadingSection.jsx
+++ src/components/LoadingSection.jsx
@@ -7,7 +7,7 @@ const LoadingSection = () => {
     width: 250,
   };
   return (
-    <div className='p-6 w-9/10 lg:w-2/4 flex flex-col justify-center items-center gap-3 text-center rounded-xl border-dotted border-4 border-gray-400 dark:border-gray-800 dark:text-gray-200 '>
+    <div className='p-6 w-9/10 lg:w-2/3 flex flex-col justify-center items-center gap-3 text-center rounded-xl border-dotted border-4 border-gray-400 dark:border-gray-800 dark:text-gray-200 '>
       <Lottie
         animationData={LoadingAnimation}
         loop={true}
